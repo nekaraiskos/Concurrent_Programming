ee5e39698d646f5167b390cd237c731a8cec6fb5
katerina <katerasla109@gmail.com>
2024-10-10 21:10:51 +0300

    "Finished" pipe.h, pipe.c, started main.c
    Ta parapanw, makefile not made, thread1 not ready, currently opening and scanning file

--- 1stAssignment/1_1/pipe.h
+++ 1stAssignment/1_1/pipe.h
@@ -5,19 +5,10 @@
 typedef struct {
     char* buffer;
     int size;
-    // char* read_ptr;
-    // char* write_ptr;
     int read_pos, write_pos;
     int id;
-    int write_open;//, read_open;
+    int write_open;
 }pipe;
-
-// extern pipe** pipe_array;
-// extern int id_num;
-// extern int num_pipes; //Contains the information about the amount of currently open pipes
-
-//Find if a pipe with id: p exists. If it exists return a pointer to it, else return a NULL pointer 
-// pipe* find_pipe(int p);
 
 //Open a new pipe for reading and writing and return its id to user
 int pipe_open(int size);
@@ -30,14 +21,5 @@ int pipe_writeDone(int p);
 
 //Read the next byte from the pipe (make it NULL after reading)
 int pipe_read(int p, char *c);
-
-//Delete an existing pipe with id: p
-// int delete_pipe(int p);
-
-//Check if a thread can write into a pipe.
-// int check_to_write(int p);
-
-// Check if the thread can read the next byte in the pipe
-// int check_to_read(int p);
 
 #endif
